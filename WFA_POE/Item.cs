0bbc0f2aa5943fb7697460b8c32054ea0c763d31
wikho <wikho.doman@gmail.com>
2022-10-12 09:41:59 +0200

    abstract Item class added with Tostring() methode

---
+++ WFA_POE/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFA_POE
+{
+    public abstract class Item : Tile
+    {
+        protected Item(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public abstract override string ToString();
+    }
+}
